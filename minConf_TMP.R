856d9f2e27b0ce41b8e475f41ff110ff85ecdc82
genwei007 <genwei007@users.noreply.github.com>
2019-02-10 00:31:34 +0800

    Add files via upload

--- minConf_TMP.R
+++ minConf_TMP.R
@@ -1,6 +1,6 @@
 minConf_TMP <- function(
   x, funObj, gr = NULL, he = NULL, LB = -Inf, UB = Inf, method = "lbfgs", ...
-  ,verbose=0,numDiff=0,optTol=1e-6
+  ,verbose=0,numDiff=1,optTol=1e-6
   ,maxIter=500,suffDec=1e-4,interp=1,corrections=100,damped=0
   ){
 
