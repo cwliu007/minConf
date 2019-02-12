dd1a86fb296652ff757e333ff25bb85326867bbb
genwei007 <genwei007@users.noreply.github.com>
2019-02-12 15:18:13 +0800

    Add files via upload

--- minConf_TMP.R
+++ minConf_TMP.R
@@ -2,17 +2,17 @@ minConf_TMP <- function(
   x, funObj, gr = NULL, he = NULL, LB = -Inf, UB = Inf, method = "lbfgs", ...
   ,verbose=0,numDiff=0,optTol=1e-6
   ,maxIter=500,suffDec=1e-4,interp=1,corrections=100,damped=0
-  ){
+){
   
   if (is.null(gr)) numDiff = 1
-
+  
   nVars = length(as.vector(x))
-
+  
   if (length(LB)==1){
     LB = rep(LB,nVars)
     UB = rep(UB,nVars)
   }
-
+  
   # Make objective function (if using numerical derivatives)
   funEvalMultiplier = 1;
   if (numDiff>0){
@@ -24,10 +24,10 @@ minConf_TMP <- function(
     # funObj = autoGrad(x,numDiff,funObj,...) 
     funEvalMultiplier = nVars+1-useComplex;
   }
-
+  
   # Evaluate Initial Point
   x = projectBounds(x,LB,UB)
-
+  
   f = funObj(x ,...)
   
   if (numDiff==0){
@@ -36,7 +36,7 @@ minConf_TMP <- function(
     g = autoGrad(x,useComplex,numDiff,funObj,...) 
   }
   g = as.vector(g)
-
+  
   if (method=="newton"){
     if (numDiff==0){
       H = he(x ,...)
@@ -48,30 +48,30 @@ minConf_TMP <- function(
     H = NULL
     secondOrder = FALSE
   }
-
+  
   funEvals = 1
-
+  
   # Compute Working Set
   working = rep(1,nVars);
   working[(x < LB+optTol*2) & g >= 0] = 0;
   working[(x > UB-optTol*2) & g <= 0] = 0;
   working = which(working != 0)
-
+  
   # Check Optimality
   if (length(working)==0){
     message = "All variables are at their bound and no further progress is possible at initial point"
     if (verbose >= 1) print(message)
-
+    
     return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
-
+    
   }else if( norm(g[working],"2") <= optTol ){
     message = "All working variables satisfy optimality condition at initial point"
     if (verbose >= 1) print(message)
-
+    
     return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
-
+    
   }
-
+  
   if (verbose >= 3){
     switch(method,
            sd = {
@@ -90,13 +90,13 @@ minConf_TMP <- function(
              message = paste("Unrecognized Method: ",method,sep="")
              return(message)
            }
-          )
+    )
   }
-
+  
   
   i = 1
   while (funEvals <= maxIter) {
-
+    
     # Compute Step Direction
     d = rep(0,nVars)
     switch(method,
@@ -132,9 +132,9 @@ minConf_TMP <- function(
              }else{
                y = g-g_old
                s = x-x_old
-
+               
                ys = t(y)%*%s
-
+               
                if (i == 2){
                  if (ys > 1e-10){
                    B = as.numeric(((t(y)%*%y)/(t(y)%*%s)))*diag(nVars)
@@ -163,8 +163,8 @@ minConf_TMP <- function(
                d[working] = - solve(H[working,working],g[working])
              }
            }
-           )
-
+    )
+    
     # Check that Progress can be made along the direction
     f_old = f
     gtd = g%*%d
@@ -173,15 +173,15 @@ minConf_TMP <- function(
       if (verbose==3) print(message)
       return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
     }
-
-
+    
+    
     # Select Initial Guess to step length
     if (i == 1 && !secondOrder){
       t = min(1,1/sum(abs(g[working])))
     }else{
       t = 1
     }
-
+    
     # Evaluate the Objective and Projected Gradient at the Initial Step
     x_new = projectBounds(x+t*d,LB,UB)
     if (secondOrder){
@@ -205,7 +205,7 @@ minConf_TMP <- function(
       g_new = as.vector(g_new)
     }
     funEvals = funEvals+1
-
+    
     ## Backtracking Line Search
     lineSearchIters = 1
     while ( (f_new > (f + suffDec*t(g)%*%(x_new-x)))  ||  (!isLegal(f_new)) ){
@@ -217,7 +217,7 @@ minConf_TMP <- function(
         if (verbose==3) print("Cubic Backtracking")
         t = polyinterp(t(matrix(c(0, f, gtd, t, f_new, t(g_new)%*%d),3,2)))$minPos
       }
-
+      
       # Adjust if change is too small
       if (t < temp*1e-3){
         if (verbose==3) print("Interpolated value too small, Adjusting")
@@ -226,7 +226,7 @@ minConf_TMP <- function(
         if (verbose==3) print("Interpolated value too large, Adjusting")
         t = temp*0.6
       }
-
+      
       # Check whether step has become too small
       if (sum(abs(t*d)) < optTol){
         message = "Line Search failed"
@@ -234,10 +234,10 @@ minConf_TMP <- function(
         t = 0
         f_new = f
         g_new = g
-
+        
         return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
       }
-
+      
       # Evaluate New Point
       x_new = projectBounds(x+t*d,LB,UB)
       f_new = funObj(x_new ,...)
@@ -250,57 +250,57 @@ minConf_TMP <- function(
       funEvals = funEvals+1
       lineSearchIters = lineSearchIters+1
     }
-
+    
     # Take Step
     x = x_new;
     f = f_new;
     g = g_new;
-
+    
     # Compute Working Set
     working = rep(1,nVars)
     working[(x < LB+optTol*2) & (g >= 0)] = 0
     working[(x > UB-optTol*2) & (g <= 0)] = 0
     working = which(working != 0)
-
+    
     # Output Log
     if (verbose>=2) print(c(as.integer(i),as.integer(funEvals*funEvalMultiplier),t,f,sum(abs(g[working]))))
-
+    
     # Check Optimality
     if (length(working)==0){
       message = "All variables are at their bound and no further progress is possible at initial point"
       if (verbose>=1) print(message)
-
+      
       return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
-
+      
     }else if( norm(g[working],"2") <= optTol ){
       message = "All working variables satisfy optimality condition at initial point"
       if (verbose>=1) print(message)
-
+      
       return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
     }
-
+    
     # Check for lack of progress
     if (sum(abs(t*d)) < optTol){
       message = "Step size below optTol"
       if (verbose>=1) print(message)
-
+      
       return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
     }
-
+    
     if (abs(f-f_old) < optTol){
       message = "Function value changing by less than optTol"
       if (verbose>=1) print(message)
-
+      
       return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
     }
-
+    
     if (funEvals*funEvalMultiplier > maxIter){
       message = "Function Evaluations exceeds maxIter"
       if (verbose>=1) print(message)
-
+      
       return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message=message)  )
     }
-
+    
     # If necessary, compute Hessian
     if (secondOrder && (lineSearchIters > 1) ){
       f_new = funObj(x ,...)
@@ -313,10 +313,10 @@ minConf_TMP <- function(
       }
       g_new = as.vector(g_new)
     }
-
+    
     i = i + 1
   }
-
+  
   return(  list(par=x,objective=f,funEvals=funEvals,g=g,H=H,message="Function Evaluations exceeds maxIter")  )
 }
 
--- minConf_TMP_autoHessian.R
+++ minConf_TMP_autoHessian.R
@@ -21,19 +21,19 @@ autoHessian <- function(x,type,numDiff,funObj,...){
     e_i = rep(0,p)
     e_j = rep(0,p)
     for (i in 1:p){
-      e_i[i] = h2
+      e_i[i] = h2[i]
       for (j in i:p){
-        e_j[j] = h2
+        e_j[j] = h2[j]
         if (i == j){
           Ui3 = funObj(x+e_i,...)
           Ui4 = funObj(x-e_i,...)
-          FD = (Ui3 - 2*logp + Ui4)/(h2^2)
+          FD = (Ui3 - 2*logp + Ui4)/(h2[i]^2)
         }else{
           Ui1 = funObj(x+e_i+e_j,...)
           Ui2 = funObj(x-e_i+e_j,...)
           Ui3 = funObj(x+e_i-e_j,...)
           Ui4 = funObj(x-e_i-e_j,...)
-          FD = (Ui1 - Ui2 - Ui3 + Ui4)/(4*h2^2)
+          FD = (Ui1 - Ui2 - Ui3 + Ui4)/(4*e_i[i]*e_j[j])
         }
         H[i,j] <- H[j,i] <- FD
         e_j[j] = 0
@@ -52,10 +52,10 @@ autoHessian <- function(x,type,numDiff,funObj,...){
     e_i = rep(0,p)
     e_j = rep(0,p)
     for (i in 1:p){
-      e_i[i] = h2
+      e_i[i] = h2[i]
       logp_k = funObj(x+e_i,...)
       for (j in i:p){
-        e_j[j] = h2
+        e_j[j] = h2[j]
         logp_h = funObj(x+e_j,...)
         
         logp_kh = funObj(x+e_i+e_j,...)
